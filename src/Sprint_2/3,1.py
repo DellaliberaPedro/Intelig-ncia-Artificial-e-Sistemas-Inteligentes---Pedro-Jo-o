c567e5273423ff93a783b3a4620e6beb6df78b9c
Pedro Henrique Devens Dellalibera <pedro.dellalibera5@yahoo.com>
2026-08-19 21:21:38 -0300

    3.1 separação e tokenização completa

--- src/Sprint_2/3,1.py
+++ src/Sprint_2/3,1.py
@@ -1,5 +1,6 @@
 import re
 
 texto = "Teste do texto, trevizol e pedro! os cara topzera."
-partes = re.split(r'(\s)', texto)
+partes = re.split(r'([,.:;?_!"()\']|--|\s)', texto)
+partes = [p.strip() for p in partes if p.strip()]
 print(partes)
