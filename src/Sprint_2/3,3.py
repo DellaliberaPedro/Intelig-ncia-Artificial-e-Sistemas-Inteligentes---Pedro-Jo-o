8b2b716ca65d491182f0d840bccb1af80e336f21
Pedro Henrique Devens Dellalibera <pedro.dellalibera5@yahoo.com>
2026-08-24 21:10:26 -0300

    Ajuste de, aplicando o BPE e gerando uma entrada e alvo

--- src/Sprint_2/3,3.py
+++ src/Sprint_2/3,3.py
@@ -6,3 +6,23 @@ texto = "Trevizol e pedro topzera demais"
 ids = tokenizer.encode(texto)
 print("Token IDs:", ids)
 print("Decodificado:", tokenizer.decode(ids))
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+caminho_arquivo = BASE_DIR / "data" / "the-verdict.txt"
+
+with open(caminho_arquivo, "r", encoding="utf-8") as f:
+    texto_completo = f.read()
+
+enc_text = tokenizer.encode(texto_completo)
+print("\nTotal de tokens (BPE) no corpus:", len(enc_text))
+
+enc_sample = enc_text[50:]
+
+context_size = 4
+x = enc_sample[:context_size]
+y = enc_sample[1:context_size + 1]
+
+print("Entrada (x):", x)
+print("Alvo    (y):", y)
