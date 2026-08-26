42dccad0350b0e7e020f723aa2fcf79d45118fd9
Pedro Henrique Devens Dellalibera <pedro.dellalibera5@yahoo.com>
2026-08-26 20:42:16 -0300

    Finalizado 3.5

--- src/Sprint_2/3,5.py
+++ src/Sprint_2/3,5.py
@@ -12,3 +12,18 @@ pos_embeddings = camada_posicional(posicoes)
 
 print("Posições:", posicoes)
 print("Formato dos embeddings posicionais:", pos_embeddings.shape)
+
+vocab_size = 50257
+output_dim = 256
+
+torch.manual_seed(123)
+camada_embedding = torch.nn.Embedding(vocab_size, output_dim)
+
+entrada_exemplo = torch.tensor([290, 4920, 2241, 287])  # mesma entrada do 3.4
+token_embeddings = camada_embedding(entrada_exemplo)
+
+input_embeddings = token_embeddings + pos_embeddings
+
+print("Formato token_embeddings:", token_embeddings.shape)
+print("Formato pos_embeddings:", pos_embeddings.shape)
+print("Formato final (token + posicional):", input_embeddings.shape)
